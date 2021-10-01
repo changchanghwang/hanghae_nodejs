a7fea9c8853807a5c70efa7c6c2c953bb16fd63f
CH_Hwang <hch950627@naver.com>
2021-10-01 14:49:58 +0900

    jest 시작

--- hanghae_node_2/nodejs-plus-shopping/middlewares/auth-middleware.js
+++ hanghae_node_2/nodejs-plus-shopping/middlewares/auth-middleware.js
@@ -1,35 +1,35 @@
-const jwt = require('jsonwebtoken');
-const {User} = require('../models');
+const jwt = require("jsonwebtoken");
+const { User } = require("../models");
 
-module.exports = async (req,res,next) =>{
-    console.log('미들웨어 지나감')
-    const { authorization } = req.headers;
-    const [tokenType, tokenValue] = authorization.split(' ');
+module.exports = async (req, res, next) => {
+  console.log("미들웨어 지나감");
+  const { authorization } = req.headers;
+  const [tokenType, tokenValue] = authorization.split(" ");
 
-    if(tokenType !== 'Bearer'){
-        res.status(401),send({
-            errorMessage: '로그인 후 사용하세요',
-        });
-        return;
-    }
+  if (tokenType !== "Bearer") {
+    res.status(401),
+      send({
+        errorMessage: "로그인 후 사용하세요",
+      });
+    return;
+  }
 
-    try{
-        const { userId } = jwt.verify(tokenValue,"secret_key");
-        User.findByPk(userId)
-            .then((user)=>{
-                res.locals.user = user;
-                next();
-            });
-        // if(!user){
-        //     res.status(401).send({
-        //         errorMessage: '로그인 후 사용하세요',
-        //     });
-        //     return;
-        // }
-    }catch (error){
-        res.status(401).send({
-            errorMessage: '로그인 후 사용하세요',
-        });
-        return;
-    }
+  try {
+    const { userId } = jwt.verify(tokenValue, "secret_key");
+    User.findByPk(userId).then((user) => {
+      res.locals.user = user;
+      next();
+    });
+    // if(!user){
+    //     res.status(401).send({
+    //         errorMessage: '로그인 후 사용하세요',
+    //     });
+    //     return;
+    // }
+  } catch (error) {
+    res.status(401).send({
+      errorMessage: "로그인 후 사용하세요",
+    });
+    return;
+  }
 };
--- hanghae_node_2/nodejs-plus-shopping/migrations/20210930140045-create-goods.js
+++ hanghae_node_2/nodejs-plus-shopping/migrations/20210930140045-create-goods.js
@@ -1,36 +1,36 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Goods', {
+    await queryInterface.createTable("Goods", {
       goodsId: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       thumbnailUrl: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       category: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       price: {
-        type: Sequelize.DECIMAL
+        type: Sequelize.DECIMAL,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Goods');
-  }
+    await queryInterface.dropTable("Goods");
+  },
 };
